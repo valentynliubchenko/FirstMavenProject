d623f6a7a01ec54bb8f4b176542038ca44ff7c69
Lyubchenko Valentin <valentin.lyubchenko@extern.a1.at>
2023-03-03 14:49:35 +0200

    add Manifest

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
+
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		if (args.length > 0) {
+			System.out.println("Hello " + args[0]);
+		} else {
+			System.out.println("Hello Annonymous");
+		}
+
+	}
 }
+
